18dc3a8bf9035171b204975fea05ff4ecafa4ee9
ytsurumis <156957587+ytsurumis@users.noreply.github.com>
2024-03-28 07:04:23 +0900

    プロトタイプコントローラーのコード修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, only: [:edit, :update, :destroy]
   before_action :move_to_index, except:[:index, :show]
   
   def index
@@ -48,8 +49,12 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+
   def move_to_index
-    unless user_signed_in?
+    unless user_signed_in? && current_user.id == @prototype.user_id
       redirect_to action: :index
     end
   end
